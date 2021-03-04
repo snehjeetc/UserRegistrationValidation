686e662e5156231520154a64ed75d5a4014cad5d
snehjeetc12 <csnehjeet12@gmail.com>
2021-03-04 23:46:31 +0530

    Added Sad (False) and Happy(True) test cases

--- src/test/java/com/userregistration/UserValidationTest.java
+++ src/test/java/com/userregistration/UserValidationTest.java
@@ -15,10 +15,24 @@ public class UserValidationTest {
     }
 
     @Test
+    public void givenFirstName_WhenNotProper_ShouldReturn_False(){
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateFirstName("snehjeet");
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void givenLastName_WhenImproper_ShouldReturn_False() {
         UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateLastName("Na");
         Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturn_True(){
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("Chatterjee");
+        Assert.assertTrue(result);
     }
 
     @Test
@@ -29,6 +43,13 @@ public class UserValidationTest {
     }
 
     @Test
+    public void givenEmail_WhenNotProper_ShouldReturn_False(){
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateEmail("abc.@gmail.com");
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void givenPhoneNumber_WhenProper_ShouldReturn_True() {
         UserValidator uservalidator = new UserValidator();
         boolean result = uservalidator.validatePhoneNumber("91 9919819801");
@@ -36,9 +57,23 @@ public class UserValidationTest {
     }
 
     @Test
+    public void givenPhoneNumber_WhenNotProper_ShouldReturn_False(){
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validatePhoneNumber("919999998888");
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void givenPassword_WhenProper_ShouldReturn_True(){
         UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validatePassword("abcA#12foRca");
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPassword_WhenNotProper_ShouldReturn_False(){
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validatePassword("abc123di");
+        Assert.assertFalse(result);
     }
 }
